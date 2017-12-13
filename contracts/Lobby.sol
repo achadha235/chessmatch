3c90db5f01a3167af0b632d091cebb2a84371260
Abhishek Chadha <abhishekchadha235@gmail.com>
2017-12-12 17:48:53 -0800

    fix compile errors

--- contracts/Lobby.sol
+++ contracts/Lobby.sol
@@ -1,1 +1,14 @@
+pragma solidity ^0.4.4;
 
+contract Lobby {
+	mapping (address => uint) deposits;
+	mapping (address => uint) pendingWithdrawals;    
+	
+	function withdraw() public {
+        uint amount = pendingWithdrawals[msg.sender];
+        // Remember to zero the pending refund before
+        // sending to prevent re-entrancy attacks
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+}
